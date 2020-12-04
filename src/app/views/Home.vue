809b2d89465dd1f4f63340fac7f68b408944558b
evan <1137299673@qq.com>
2020-12-04 17:53:29 +0800

    move all app files to /app folder

--- src/app/views/Home.vue
+++ src/app/views/Home.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import HelloWorld from '@/app/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',

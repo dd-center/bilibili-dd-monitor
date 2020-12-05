f048f77609d36b752b136f584bd4b7a2e83ab58b
evan <1137299673@qq.com>
2020-12-06 01:06:29 +0800

    里程碑：引入services代码。build通过。这次 v8 没有 crashes，chromium 耗子尾汁

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, ipcMain } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 import path from 'path'
@@ -22,7 +22,7 @@ protocol.registerSchemesAsPrivileged([
 
 async function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -36,15 +36,108 @@ async function createWindow () {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
+    await mainWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
     // await win.loadURL('https://www.bilibili.com/blackboard/live/live-activity-player.html?enterTheRoom=0&cid=21396545')
 
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (!process.env.IS_TEST) mainWindow.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    await mainWindow.loadURL('app://./index.html')
   }
+
+  return mainWindow
+}
+
+const initUpdate = () => {
+  // noop
+}
+
+const initSettingsConfiguration = () => {
+  settings.configure({
+    numSpaces: 2,
+    prettify: true
+  })
+}
+
+const initServices = () => {
+  vtbInfosService = new VtbInfoService()
+  mainWindow.on('close', () => {
+    // clean up vtbInfo update
+    vtbInfosService.stopUpdate()
+  })
+
+  FollowListService.initFollowListsSync()
+}
+
+const initIpcMainListeners = () => {
+  // region VtbInfo
+  ipcMain.on('getVtbInfos', (event: Electron.IpcMainEvent) => {
+    event.reply('getVtbInfosReply', vtbInfosService.getVtbInfosMock())
+  })
+  ipcMain.on('getFollowedVtbInfos', (event: Electron.IpcMainEvent) => {
+    event.reply('getFollowedVtbInfosReply', vtbInfosService.getFollowedVtbInfosMock())
+  })
+  // endregion
+
+  // region notification
+  ipcMain.on('setIsNotifiedOnStart', (event: Electron.IpcMainEvent, isNotifiedOnStart: boolean) => {
+    event.reply('setIsNotifiedOnStartReply', SettingService.setIsNotifiedOnStartSync(isNotifiedOnStart))
+  })
+  ipcMain.on('getIsNotifiedOnStart', (event: Electron.IpcMainEvent) => {
+    event.reply('getIsNotifiedOnStartReply', SettingService.getIsNotifiedOnStartSync())
+  })
+  // endregion
+
+  // region follow
+  ipcMain.on('getFollowedVtbMids', (event: Electron.IpcMainEvent) => {
+    event.reply('getFollowedVtbMidsReply', FollowListService.getFollowedVtbMidsSync())
+  })
+  ipcMain.on('getFollowLists', (event: Electron.IpcMainEvent) => {
+    event.reply('getFollowListsReply', FollowListService.getFollowListsSync())
+  })
+  ipcMain.on('addFollowList', (event: Electron.IpcMainEvent, name: string) => {
+    FollowListService.addFollowListSync(name)
+    event.reply('addFollowListReply', FollowListService.getFollowListsSync())
+  })
+  ipcMain.on('deleteFollowList', (event: Electron.IpcMainEvent, id: number) => {
+    FollowListService.deleteFollowListSync(id)
+    event.reply('deleteFollowListReply', FollowListService.getFollowListsSync())
+  })
+  ipcMain.on('renameFollowList', (event: Electron.IpcMainEvent, id: number, newName: string) => {
+    FollowListService.renameFollowListSync(id, newName)
+    event.reply('renameFollowListReply', FollowListService.getFollowListsSync())
+  })
+  ipcMain.on('toggleFollow', (event: Electron.IpcMainEvent, mid: number) => {
+    FollowListService.toggleFollowSync(mid)
+    event.reply('toggleFollowReply', FollowListService.getFollowedVtbMidsSync())
+  })
+  ipcMain.on('setFollowList', (event: Electron.IpcMainEvent, mids: number[], listId: number) => {
+    FollowListService.addMidsToFollowListSync(mids, listId)
+    event.reply('setFollowListReply', FollowListService.getFollowListsSync())
+  })
+  // endregion
+
+  // region player
+  ipcMain.on('showPlayer', (event: Electron.IpcMainEvent, roomid: number) => {
+    if (playerObjMap.has(roomid)) {
+      playerObjMap.get(roomid)!.playerWindow.focus()
+    } else {
+      const vtbInfo: VtbInfo = vtbInfosService.getVtbInfosMock().find((vtbInfo: VtbInfo) => {
+        return vtbInfo.roomid === roomid
+      })!
+      playerObjMap.set(roomid, createPlayerWindow(app, vtbInfo, playerObjMap))
+    }
+  })
+  // endregion
+}
+
+const initApp = () => {
+  initUpdate()
+  initSettingsConfiguration()
+  initServices()
+  // todo 上播提醒
+  initIpcMainListeners()
 }
 
 // Quit when all windows are closed.
@@ -56,10 +149,12 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
+app.on('activate', async () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = await createWindow()
+  }
 })
 
 // This method will be called when Electron has finished
@@ -74,7 +169,8 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
-  createWindow()
+  mainWindow = await createWindow()
+  initApp()
 })
 
 // Exit cleanly on request from parent process in development mode.
